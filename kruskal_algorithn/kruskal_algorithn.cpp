bf31ea6c4566bc7d669c2611b46025cbb13926b8
lluxfero <onastja02@gmail.com>
2022-05-25 23:52:39 +0300

    Добавьте файлы проекта.

---
+++ kruskal_algorithn/kruskal_algorithn.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <clocale>
+#include <vector>
+using namespace std;
+
+const int n = 12;
+
+void recolor(int colors[n], int c1, int c2) {
+	for (int i = 0; i < n; i++) {
+		if (colors[i] == c2) {
+			colors[i] = c1;
+		}
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int edges[n][3] = {
+		{12, 1, 2},
+		{16, 1, 6},
+		{14, 1, 7},
+		{10, 2, 3},
+		{7, 2, 6},
+		{3, 3, 4},
+		{5, 3, 5},
+		{6, 3, 6},
+		{4, 4, 5},
+		{2, 5, 6},
+		{8, 5, 7},
+		{9, 6, 7}, };
+
+	int colors[n];
+	for (int i = 0; i < n; i++) {
+		colors[i] = i;
+	}
+	
+	for (int i = 0; i < n - 1; i++)
+		for (int j = 0; j < n - i - 1; j++)
+			if (edges[j][0] > edges[j + 1][0])
+				for (int k = 0; k < 3; k++)
+					swap(edges[j][k], edges[j + 1][k]);
+
+	int result[n][3];
+	int x = 0;
+	for (int i = 0; i < n; i++) {
+		if (colors[edges[i][1] - 1] != colors[edges[i][2] - 1]) {
+			recolor(colors, colors[edges[i][1] - 1], colors[edges[i][2] - 1]);
+			result[x][0] = edges[i][0];
+			result[x][1] = edges[i][1];
+			result[x][2] = edges[i][2];
+			x++;
+		}
+	}
+	cout << "Алгоритм Краскала: " << endl;
+	for (int i = 0; i < x; i++) {
+		cout << "из " << result[i][1] << " в " << result[i][2] << ", вес: " << result[i][0] << endl;
+	}
+}
